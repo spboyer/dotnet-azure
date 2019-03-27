4fb922dd149da0e2549e479f7616a24df9b0451c
Shayne Boyer <spboyer@live.com>
2019-03-26 20:55:31 -0700

    initial command struct

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace azure
 {
-    class Program
+
+  [Command(Name = "azure-deploy", Description = "A global command set to deploy .NET Core apps to Azure."),
+       Subcommand(typeof(Deploy))]
+  class Azure
+  {
+    public static void Main(string[] args) => CommandLineApplication.Execute<Azure>(args);
+
+    private int OnExecute(CommandLineApplication app, IConsole console)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      console.WriteLine("You must specify at a subcommand.");
+      app.ShowHelp();
+      return 1;
     }
+
+    [Command("deploy", Description = "Deploy application to Azure App Service")]
+    private class Deploy
+    {
+      private IReadOnlyList<string> RemainingArguments { get; }
+      private void OnExecute(IConsole console)
+      {
+        // do action here
+      }
+
+    }
+  }
 }
