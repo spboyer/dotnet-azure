dad49a53db7d3997f1c7b9865ddaa9d151aa2fa1
Shayne Boyer <spboyer@live.com>
2019-03-27 14:52:58 -0700

    sdk add

--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
 
 namespace dotnet_azure
 {
 
   [Command(Name = "dotnet azure", Description = "A global command set to deploy .NET Core apps to Azure."),
        Subcommand(typeof(Deploy), typeof(GetAzure))]
-  class Azure
+  class App
   {
     public static void Main(string[] args) => CommandLineApplication.Execute<Azure>(args);
 
-    [Argument(0)]
-    public string AppPath { get; set; } = "./";
 
     private int OnExecute(CommandLineApplication app, IConsole console)
     {
@@ -24,11 +28,60 @@ namespace dotnet_azure
     [Command("deploy", Description = "Deploy application to Azure App Service")]
     private class Deploy
     {
+
+      [Argument(0)]
+      public string AppPath { get; set; } = "./";
+
       private IReadOnlyList<string> RemainingArguments { get; }
       private void OnExecute(IConsole console)
       {
         // do action here
       }
+
+      public IAzure GetSdkClient()
+      {
+
+        if (!File.Exists(CLI.fileName))
+        {
+          CLI.CreateServicePrincipalFile();
+        }
+
+        var credentials = SdkContext.AzureCredentialsFactory.FromFile(CLI.fileName);
+
+        return Azure
+            .Configure()
+            .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+            .Authenticate(credentials)
+            .WithDefaultSubscription();
+      }
+
+      public async Task CreateWebApp(string regionName = "southcentralus")
+      {
+        var appName = Guid.NewGuid().ToString();
+        var nm = string.Format($"aspnetcoreapp{appName}");
+        Console.WriteLine($"Creating web app {nm}");
+
+        try
+        {
+          var webApp = await GetSdkClient()
+              .WebApps
+                  .Define(nm)
+                  .WithRegion(
+                      Region.Create(regionName)
+                  )
+                  .WithNewResourceGroup(nm)
+                  .WithNewWindowsPlan(pricingTier: PricingTier.BasicB1)
+                  .CreateAsync();
+
+          Console.WriteLine($"Created web app {nm}");
+        }
+        catch (Exception ex)
+        {
+          Console.WriteLine($"Error creating web app {nm} - " + ex.Message);
+          throw ex; // throwing so caller can handle
+        }
+      }
+
 
     }
 
