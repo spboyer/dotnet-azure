1d6e0a453e29466e1e8e3519ed7812eb94ca3cc6
Shayne Boyer <spboyer@live.com>
2019-04-18 12:29:26 -0400

    nate help

--- Commands/Login.cs
+++ Commands/Login.cs
@@ -3,6 +3,8 @@ using McMaster.Extensions.CommandLineUtils;
 using Microsoft.AspNetCore.NodeServices;
 using Microsoft.Extensions.DependencyInjection;
 using System.IO;
+using System.Reflection;
+using System;
 
 namespace dotnet_azure
 {
@@ -36,7 +38,7 @@ namespace dotnet_azure
     private static async Task<string> DoLogin(INodeServices nodeService)
     {
       //Invoke the javascript module with parameters to execute in Node environment.
-      return await nodeService.InvokeAsync<string>(@"scripts/auth.js");
+      return await nodeService.InvokeAsync<string>(Path.Combine(System.AppContext.BaseDirectory, "scripts", "auth.js"));
     }
   }
 }
