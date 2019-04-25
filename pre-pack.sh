b5416ddabfe569cfa60b9a18e7b325ef6275f842
Shayne Boyer <spboyer@live.com>
2019-04-25 15:09:10 -0400

    mv path change

--- pre-pack.sh
+++ pre-pack.sh
@@ -7,6 +7,6 @@
 find node_modules/adal-node -type f -exec sed -i '' 's/xpath.js/xpathjs/g' {} \;
 
 # rename the xpath.js npm package folder and name
-mv node_modules/xpath.js ./node_modules/xpathjs
+mv node_modules/xpath.js node_modules/xpathjs
 # change the name in the package.json to reflect naming change
 find node_modules/xpathjs -type f -exec sed -i '' 's/xpath.js/xpathjs/g' {} \;
