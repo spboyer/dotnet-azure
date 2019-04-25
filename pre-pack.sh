54fd68cb3aaabb9a2446a41449576c215065f2ce
Shayne Boyer <spboyer@live.com>
2019-04-25 15:08:34 -0400

    change path for pre-pack

--- pre-pack.sh
+++ pre-pack.sh
@@ -4,9 +4,9 @@
 
 # rename npm package reference in adal-node
 # covers package.json and any file referencing package
-find ./node_modules/adal-node -type f -exec sed -i '' 's/xpath.js/xpathjs/g' {} \;
+find node_modules/adal-node -type f -exec sed -i '' 's/xpath.js/xpathjs/g' {} \;
 
 # rename the xpath.js npm package folder and name
-mv ./node_modules/xpath.js ./node_modules/xpathjs
+mv node_modules/xpath.js ./node_modules/xpathjs
 # change the name in the package.json to reflect naming change
-find ./node_modules/xpathjs -type f -exec sed -i '' 's/xpath.js/xpathjs/g' {} \;
+find node_modules/xpathjs -type f -exec sed -i '' 's/xpath.js/xpathjs/g' {} \;
